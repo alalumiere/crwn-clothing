70d27f1405acae4dd866fff78a85ffa0df0fb5c0
Amanda LaLumiere <amandalalumiere@gmail.com>
2020-12-07 17:12:42 -0600

    create collections overview component as a nested component of shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,11 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import React, { collections } from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import "./shop.styles.scss";
 
 const shopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />;
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(shopPage);
+export default shopPage;
